016c132087bc824f6955cac0c93e1912ab12c47a
Batuhan Budak <batuhan.budak@gmail.com>
2023-01-17 21:44:59 +0300

    Create handlers and view templates for create,  and read operations

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,41 +1,174 @@
 const Category = require("../models/category");
+const Item = require("../models/item");
+const async = require("async");
+const { body, validationResult } = require("express-validator");
 
-// Display list of all Authors.
-exports.category_list = (req, res) => {
-  res.send("NOT IMPLEMENTED: Author list");
+// Display list of all Categories.
+exports.category_list = (req, res, next) => {
+  Category.find()
+    .sort([["name", "ascending"]])
+    .exec(function (err, list_category) {
+      if (err) {
+        return next(err);
+      }
+      res.render("category_list", {
+        title: "Category List",
+        category_list: list_category,
+      });
+    });
 };
 
-// Display detail page for a specific Author.
-exports.category_detail = (req, res) => {
-  res.send(`NOT IMPLEMENTED: Author detail: ${req.params.id}`);
+// Display detail page for a specific Category.
+exports.category_detail = (req, res, next) => {
+  async.parallel(
+    {
+      category(callback) {
+        Category.findById(req.params.id).exec(callback);
+      },
+      category_items(callback) {
+        Item.find({ category: req.params.id }).exec(callback);
+      },
+    },
+    (err, results) => {
+      if (err) {
+        return next(err);
+      }
+      if (results.category === null) {
+        const error = new Error("Category not found");
+        error.status = 404;
+        return next(error);
+      }
+      res.render("category_detail", {
+        title: "Category Detail",
+        category: results.category,
+        category_items: results.category_items,
+      });
+    }
+  );
 };
 
-// Display Author create form on GET.
+// Display Category create form on GET.
 exports.category_create_get = (req, res) => {
-  res.send("NOT IMPLEMENTED: Author create GET");
+  res.render("category_form", {
+    title: "Create Category",
+  });
 };
 
-// Handle Author create on POST.
-exports.category_create_post = (req, res) => {
-  res.send("NOT IMPLEMENTED: Author create POST");
+// Handle Category create on POST.
+exports.category_create_post = [
+  body("name", "Category name is required")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("description", "Category description is required")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  (req, res, next) => {
+    const errors = validationResult(req);
+
+    const category = new Category({
+      name: req.body.name,
+      description: req.body.description,
+    });
+
+    if (!errors.isEmpty()) {
+      res.render("category_form", {
+        title: "Create Category",
+        category,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      Category.findOne({ name: req.body.name }).exec((err, found_category) => {
+        if (err) {
+          return next(err);
+        }
+        if (found_category) {
+          res.redirect(found_category.url);
+        } else {
+          category.save((err) => {
+            if (err) {
+              return next(err);
+            }
+            res.redirect(category.url);
+          });
+        }
+      });
+    }
+  },
+];
+
+// Display Category delete form on GET.
+exports.category_delete_get = (req, res, next) => {
+  async.parallel(
+    {
+      items(callback) {
+        Item.find({ category: req.params.id }).exec(callback);
+      },
+      category(callback) {
+        Category.findById(req.params.id).exec(callback);
+      },
+    },
+    (err, results) => {
+      if (err) {
+        return next(err);
+      }
+      if (results.category === null) {
+        res.redirect("/catalog/categories");
+      }
+      res.render("category_delete", {
+        title: "Delete Category",
+        category: results.category,
+        items: results.items,
+      });
+    }
+  );
 };
 
-// Display Author delete form on GET.
-exports.category_delete_get = (req, res) => {
-  res.send("NOT IMPLEMENTED: Author delete GET");
+// Handle Category delete on POST.
+exports.category_delete_post = (req, res, next) => {
+  async.parallel(
+    {
+      items(callback) {
+        Item.find({ category: req.body.categoryid }).exec(callback);
+      },
+      category(callback) {
+        Category.findById(req.body.categoryid).exec(callback);
+      },
+    },
+    (err, results) => {
+      if (err) {
+        return next(err);
+      }
+      if (results.category === null) {
+        res.redirect("/catalog/categories");
+      }
+
+      if (results.items.length > 0) {
+        res.render("category_delete", {
+          title: "Delete Category",
+          category: results.category,
+          items: results.items,
+        });
+        return;
+      }
+      Category.findByIdAndRemove(req.body.categoryid, (err) => {
+        if (err) {
+          return next(err);
+        }
+        res.redirect("/catalog/categories");
+      });
+    }
+  );
 };
 
-// Handle Author delete on POST.
-exports.category_delete_post = (req, res) => {
-  res.send("NOT IMPLEMENTED: Author delete POST");
-};
-
-// Display Author update form on GET.
+// Display Category update form on GET.
 exports.category_update_get = (req, res) => {
-  res.send("NOT IMPLEMENTED: Author update GET");
+  res.send("NOT IMPLEMENTED: Category update GET");
 };
 
-// Handle Author update on POST.
+// Handle Category update on POST.
 exports.category_update_post = (req, res) => {
-  res.send("NOT IMPLEMENTED: Author update POST");
+  res.send("NOT IMPLEMENTED: Category update POST");
 };
--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,6 +1,7 @@
 const Item = require("../models/item");
 const Category = require("../models/category");
 const async = require("async");
+const { body, validationResult } = require("express-validator");
 
 exports.index = (req, res) => {
   async.parallel(
@@ -26,7 +27,7 @@ exports.index = (req, res) => {
   );
 };
 
-// Display list of all Authors.
+// Display list of all Items.
 exports.item_list = (req, res, next) => {
   Item.find({}, "name description")
     .sort({ name: 1 })
@@ -42,37 +43,132 @@ exports.item_list = (req, res, next) => {
     });
 };
 
-// Display detail page for a specific Author.
-exports.item_detail = (req, res) => {
-  res.send(`NOT IMPLEMENTED: Author detail: ${req.params.id}`);
+// Display detail page for a specific Item.
+exports.item_detail = (req, res, next) => {
+  Item.findById(req.params.id)
+    .populate("category")
+    .exec(function (err, item) {
+      if (err) {
+        return next(err);
+      }
+      if (item === null) {
+        const err = new Error("Item not found");
+        err.status = 404;
+        return next(err);
+      }
+      res.render("item_detail", {
+        item,
+      });
+    });
 };
 
-// Display Author create form on GET.
-exports.item_create_get = (req, res) => {
-  res.send("NOT IMPLEMENTED: Author create GET");
+// Display Item create form on GET.
+exports.item_create_get = (req, res, next) => {
+  Category.find({}).exec(function (err, categories) {
+    if (err) {
+      return next(err);
+    }
+    res.render("item_form", { title: "Create Item", categories });
+  });
 };
 
-// Handle Author create on POST.
-exports.item_create_post = (req, res) => {
-  res.send("NOT IMPLEMENTED: Author create POST");
+// Handle Item create on POST.
+exports.item_create_post = [
+  (req, res, next) => {
+    if (!Array.isArray(req.body.category)) {
+      req.body.category =
+        typeof req.body.category === "undefined" ? [] : [req.body.category];
+    }
+    next();
+  },
+
+  body("name", "Item name is required").trim().isLength({ min: 1 }).escape(),
+  body("description", "Item description is required")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("price", "Item price is required")
+    .trim()
+    .isDecimal({ decimal_digits: "2" })
+    .isFloat({ min: 0.1 })
+    .escape(),
+  body("number_in_stock", "Stock amount is required")
+    .trim()
+    .isNumeric()
+    .isInt({ gt: 0 })
+    .escape(),
+  body("category.*").escape(),
+  (req, res, next) => {
+    const errors = validationResult(req);
+
+    const item = new Item({
+      name: req.body.name,
+      description: req.body.description,
+      price: req.body.price,
+      number_in_stock: req.body.number_in_stock,
+      category: req.body.category,
+    });
+
+    if (!errors.isEmpty()) {
+      Category.find({}).exec((err, categories) => {
+        if (err) {
+          return next(err);
+        }
+        for (const category of categories) {
+          if (item.category.includes(category._id)) {
+            category.checked = true;
+          }
+        }
+        res.render("item_form", {
+          title: "Create Item",
+          item,
+          categories,
+          errors: errors.array(),
+        });
+      });
+      return;
+    }
+    item.save((err) => {
+      if (err) {
+        return next(err);
+      }
+      res.redirect(item.url);
+    });
+  },
+];
+
+// Display Item delete form on GET.
+exports.item_delete_get = (req, res, next) => {
+  Item.findById(req.params.id).exec((err, item) => {
+    if (err) {
+      return next(err);
+    }
+    if (item === null) {
+      res.redirect("/catalog/items");
+    }
+    res.render("item_delete", {
+      title: "Delete item",
+      item,
+    });
+  });
 };
 
-// Display Author delete form on GET.
-exports.item_delete_get = (req, res) => {
-  res.send("NOT IMPLEMENTED: Author delete GET");
-};
-
-// Handle Author delete on POST.
+// Handle Item delete on POST.
 exports.item_delete_post = (req, res) => {
-  res.send("NOT IMPLEMENTED: Author delete POST");
+  Item.findByIdAndRemove(req.body.itemid, (err) => {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/catalog/items");
+  });
 };
 
-// Display Author update form on GET.
+// Display Item update form on GET.
 exports.item_update_get = (req, res) => {
-  res.send("NOT IMPLEMENTED: Author update GET");
+  res.send("NOT IMPLEMENTED: Item update GET");
 };
 
-// Handle Author update on POST.
+// Handle Item update on POST.
 exports.item_update_post = (req, res) => {
-  res.send("NOT IMPLEMENTED: Author update POST");
+  res.send("NOT IMPLEMENTED: Item update POST");
 };
